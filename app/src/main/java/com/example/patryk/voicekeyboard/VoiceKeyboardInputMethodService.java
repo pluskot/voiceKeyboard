5aacc5d843f894f21572cbccd9635372ada4d9e2
ktm_631 <ktm631@gmail.com>
2018-12-27 21:46:08 +0100

    Keyboard layout ready

--- app/src/main/java/com/example/patryk/voicekeyboard/VoiceKeyboardInputMethodService.java
+++ app/src/main/java/com/example/patryk/voicekeyboard/VoiceKeyboardInputMethodService.java
@@ -1,69 +1,16 @@
 package com.example.patryk.voicekeyboard;
 
-import android.content.Intent;
-import android.inputmethodservice.KeyboardView;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
-import android.inputmethodservice.KeyboardView;
 import android.view.View;
+import android.widget.FrameLayout;
 
-public class VoiceKeyboardInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener{
-    private KeyboardView keyboardView;
-    private Keyboard keyboard;
+public class VoiceKeyboardInputMethodService extends InputMethodService{
+    private FrameLayout keyboardView;
 
     @Override
     public View onCreateInputView() {
-        keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
-        keyboard = new Keyboard(this, R.xml.keyboard_layout);
-        keyboardView.setKeyboard(keyboard);
-        keyboardView.setOnKeyboardActionListener(this);
+        keyboardView = (FrameLayout) getLayoutInflater().inflate(R.layout.keyboard_view, null);
         return keyboardView;
-    }
-
-    @Override
-    public void onPress(int primaryCode) {
-
-    }
-
-    @Override
-    public void onRelease(int primaryCode) {
-
-    }
-
-    @Override
-    public void onKey(int primaryCode, int[] keyCodes) {
-        switch (primaryCode) {
-            case 1:
-                VoiceKeyboardActivity.inputConnection = getCurrentInputConnection();
-                Intent intent = new Intent(getApplicationContext(), VoiceKeyboardActivity.class);
-                intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                getCurrentInputConnection().commitText(VoiceKeyboardActivity.result, 1);
-        }
-    }
-
-    @Override
-    public void onText(CharSequence text) {
-
-    }
-
-    @Override
-    public void swipeLeft() {
-
-    }
-
-    @Override
-    public void swipeRight() {
-
-    }
-
-    @Override
-    public void swipeDown() {
-
-    }
-
-    @Override
-    public void swipeUp() {
-
     }
 }
